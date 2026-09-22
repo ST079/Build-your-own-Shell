05e777f549cbcf2005be326005fc92c9a1149eae
Sujan Tamang <sujan.tamang@veelapp.com>
2026-09-23 02:58:37 +0545

    codecrafters submit [skip ci]

--- src/Commands/CdCommand.cs
+++ src/Commands/CdCommand.cs
@@ -4,11 +4,16 @@ public class CdCommand
 {
     public void Execute(string[] arguments)
     {
+        if (arguments.Length == 0)
+        {
+            return;
+        }
+
         string path = arguments[0];
 
         if (path == "~")
         {
-            path = Environment.GetEnvironmentVariable("Home") ?? "";
+            path = Environment.GetEnvironmentVariable("HOME") ?? "";
         }
 
         if (Directory.Exists(path))
