443d0bf4ccdc8e7a4135006d4a1f6de2fdbdc2b8
Sujan Tamang <sujan.tamang@veelapp.com>
2026-09-26 11:28:55 +0545

    codecrafters submit [skip ci]

--- src/Shell/CommandParser.cs
+++ src/Shell/CommandParser.cs
@@ -4,11 +4,51 @@ public class CommandParser
 {
     public ParsedCommand Parse(string input)
     {
-        var parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        // var parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        // return new ParsedCommand
+        // {
+        //     Name = parts[0],
+        //     Arguments = parts.Skip(1).ToArray(),
+        // };
+
+        var tokens = new List<string>();
+        var current = new List<char>();
+
+        bool insideSingleQuotes = false;
+
+        foreach (char character in input)
+        {
+
+            if (Equals(character, '\''))
+            {
+                insideSingleQuotes = !insideSingleQuotes;
+                continue;
+            }
+
+            if( char.IsWhiteSpace(character) && !insideSingleQuotes)
+            {
+                if (current.Count > 0)
+                {
+                    tokens.Add(new string(current.ToArray()));
+                    current.Clear();
+                }
+                continue;
+            }
+
+            current.Add(character);
+        }
+
+        if(current.Count > 0)
+        {
+            tokens.Add(new string(current.ToArray()));
+        }
+
         return new ParsedCommand
         {
-            Name = parts[0],
-            Arguments = parts.Skip(1).ToArray(),
+            Name = tokens[0],
+            Arguments = tokens.Skip(1).ToArray(),
         };
+
+        
     }
 }
